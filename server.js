1e87b79cb264a51c5fa41ab7a6b8adf0d6904dd9
Kaitlyn Branagan <kaitlynwebb_46@hotmail.com>
2020-11-26 21:19:44 -0700

    adding the css static sheet

---
+++ server.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const PORT = process.env.PORT || 3000;
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join('public')));
+
+
+
+
+app.get('/notes', function(req,res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+});
+
+app.get('*', function(req,res) {
+res.sendFile(path.join(__dirname, "public/index.html"))
+
+})
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
+
